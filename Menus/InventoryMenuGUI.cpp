3061a6e9820976932aa7896c526c516729cfdc07
Wazubaba <wazubaba@no.you>
2022-03-29 02:50:27 -0500

    Quick enhancement to CTD fix code

--- Menus/InventoryMenuGUI.cpp
+++ Menus/InventoryMenuGUI.cpp
@@ -384,11 +384,12 @@ namespace RTE {
 
 		switch (m_MenuMode) {
 			case MenuMode::Carousel:
+
+				// Separate this check - former bit can be false but latter true and thus cause a CTD
 				if (!m_InventoryActor || m_InventoryActor->IsInventoryEmpty()) {
 					return;
 				}
-				// Logically this should no longer be needed
-				if (!m_InventoryActor || (m_InventoryActor->IsInventoryEmpty() && m_InventoryActorEquippedItems.empty() && m_EnabledState != EnabledState::Disabling)) {
+				if (m_InventoryActorEquippedItems.empty() && m_EnabledState != EnabledState::Disabling) {
 					return;
 				}
 				drawPos -= Vector(0, c_CarouselMenuVerticalOffset + c_CarouselBoxMaxSize.GetY() * 0.5F);
