628a6c2ddc5ec28d0b3ef03bfba86cc7a3f04f16
Wazubaba <wazubaba@no.you>
2022-05-24 20:55:01 -0500

    Fix small bug from last commit

--- Lua/LuaBindingsEntities.cpp
+++ Lua/LuaBindingsEntities.cpp
@@ -237,8 +237,8 @@ namespace RTE {
 		.property("MovePathSize", &Actor::GetMovePathSize)
 		.property("AimDistance", &Actor::GetAimDistance, &Actor::SetAimDistance)
 		.property("SightDistance", &Actor::GetSightDistance, &Actor::SetSightDistance)
-		.property("IsRobotic", &Actor::GetRobotic, &Actor::SetRobotic)
-		.property("IsOrganic", &Actor::GetOrganic, &Actor::SetOrganic)
+		.property("Robotic", &Actor::GetRobotic, &Actor::SetRobotic)
+		.property("Organic", &Actor::GetOrganic, &Actor::SetOrganic)
 
 		.def_readwrite("MOMoveTarget", &Actor::m_pMOMoveTarget)
 		.def_readwrite("MovePath", &Actor::m_MovePath, luabind::return_stl_iterator)
